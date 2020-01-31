0efb462992746fe153fd9bff06e3f94e76a87d0c
MZ <mantas141@gmail.com>
2020-01-31 16:29:38 +0100

    Prices log navigation set

--- prices_log/link.php
+++ prices_log/link.php
@@ -1,6 +1,16 @@
 <?php 
 $id = $_REQUEST['page'];
-echo $id;
+switch($id){
+    case '1':
+        include('./prices_log/log.inc');
+        break;
+    case '2':
+        include('./prices_log/Compare.inc');
+        break;
+    case '3':
+        include('./prices_log/Items.inc');
+        break;
+}
 
 
 ?>
